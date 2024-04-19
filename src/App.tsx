985ef58985236cdcb68c60800406cff6a0661e87
Henry Agu <aguhenrychuks@gmail.com>
2024-04-19 09:09:41 +0100

    feat: result info added

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,52 @@ import { useState } from "react";
 
 function App() {
   const [percentage, setPercentage] = useState(0);
+  const [result, setResult] = useState("");
   const [loading, setLoading] = useState(false);
   const [yourName, setYourName] = useState("");
   const [crushName, setCrushName] = useState("");
   // @ts-ignore
-  const handleLoveCalculation = async (e) =>{
+  const handleLoveCalculation = async (e) => {
     e.preventDefault();
     setLoading(true);
-    await new Promise((resolve) => setTimeout(resolve, 1000)); 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const newNumber = Math.floor(Math.random() * 100) + 1;
     setPercentage(newNumber);
     setLoading(false);
-  }
+
+    if (percentage <= 10) {
+      setResult(
+        `Hmm, it seems like there's a bit of distance between ${yourName} and ${crushName}. Keep your hopes high, miracles happen!`
+      );
+    } else if (percentage <= 35) {
+      setResult(
+        `Well, ${yourName}, there's some potential spark between you and ${crushName}. Don't hesitate to make a move and see where it leads!`
+      );
+    } else if (percentage <= 65) {
+      setResult(
+        `Looks like there's a good chance for love to blossom between ${yourName} and ${crushName}! Take the initiative and see where this journey takes you both!`
+      );
+    } else if (percentage <= 85) {
+      setResult(
+        `Wow! With such a high percentage, it's clear that there's a strong connection between ${yourName} and ${crushName}. Don't hold back, seize the moment!`
+      );
+    } else if (percentage <= 100) {
+      setResult(
+        `Congratulations, ${yourName}! You and ${crushName} are a match made in heaven! It's time to celebrate and embrace this beautiful connection!`
+      );
+    }
+  };
   return (
     <main className="flex flex-col gap-y-5 items-center justify-center w-full h-screen">
       <div>
         <h1 className="font-custom italic text-center text-4xl text-[#e85c8b] font-bold">
           Tiny-Love Calculator
         </h1>
-        <form action="" className="flex flex-col items-center mt-5" onSubmit={handleLoveCalculation}>
+        <form
+          action=""
+          className="flex flex-col items-center mt-5"
+          onSubmit={handleLoveCalculation}
+        >
           <div className="flex flex-col my-0 mx-10 gap-y-3.5 lg:flex-row lg:gap-x-2.5 items-center">
             <div className="flex flex-col gap-y-1.5">
               <label htmlFor="">Your Name</label>
@@ -29,7 +56,7 @@ function App() {
                 required
                 placeholder="Enter your name"
                 value={yourName}
-                onChange={(e)=> setYourName(e.target.value)}
+                onChange={(e) => setYourName(e.target.value)}
                 className=" py-2 px-3 w-[90vw] lg:w-[30vw] border-2 border-[#e85c8b] rounded-md outline-[#e85c8b]"
               />
             </div>
@@ -40,7 +67,7 @@ function App() {
                 required
                 placeholder="Enter your crush's name"
                 value={crushName}
-                onChange={(e)=> setCrushName(e.target.value)}
+                onChange={(e) => setCrushName(e.target.value)}
                 className="py-2 p-3 w-[90vw] lg:w-[30vw] border-2 border-[#e85c8b] rounded-md outline-[#e85c8b]"
               />
             </div>
@@ -54,6 +81,11 @@ function App() {
         <h1 className="font-custom text-8xl font-semibold text-[#e85c8b]">
           {percentage}%
         </h1>
+      </div>
+      <div className="text-center mt-5 w-[95%]">
+        <p className="font-custom font-medium italic text-[#e85c8b] text-xl">
+          {result}
+        </p>
       </div>
     </main>
   );
